0f38d04ab015c69e4e624e3386e120212a4e7b42
frank.yao <frank.yao@vipshop.com>
2015-11-03 10:46:13 +0800

    fix dash bug in path

--- hackit.py
+++ hackit.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     n = sys.argv[1]
     resp = requests.get('http://pu.guitarworld.com.cn/q%s/' % n)
     r = re.findall('<title>(.*)</title>', resp.content)
-    title = r[0].split('-')[0].strip()
+    title = r[0].split('-')[0].strip().replace('/', '_')
     for index, (song_id, page_id) in enumerate(re.findall('/userspace/uploadfiles/qupupic/(\d+)/thumbnail/(\d+).gif', resp.content)):
-        print song_id, page_id
+        print song_id, page_id, title
         url = 'http://pu.guitarworld.com.cn/userspace/uploadfiles/qupupic/%s/%s.gif' % (song_id, page_id)
         ret = requests.get(url)
         file_name = title + '_' + str(index + 1) + '.gif'
